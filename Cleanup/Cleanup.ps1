b12166dd429665e80731037c2d8715337a44f4a0
Lemoine Kris <lemoinek@pandora.be>
2016-09-15 09:19:46 +0200

    Add files via upload

---
+++ Cleanup/Cleanup.ps1
@@ -0,0 +1,25 @@
+﻿param (
+[string]$path ,
+[int]$retentionDays,
+[int]$minimumToKeep
+)
+
+$folderlist = Get-ChildItem $path | sort @{Expression={$_.LastWriteTime}; Ascending=$false}
+
+$keepDate = (Get-Date).AddDays(-$retentionDays)
+$keeping = 0
+
+ForEach ($folder In $folderlist) {
+    
+    if($folder.LastWriteTime -lt $keepDate -And $minimumToKeep -le $keeping)
+    {
+       Write-Output "Deleting $($folder.FullName)"
+       Remove-Item $folder.FullName
+    }
+    else
+    {
+        $keeping++
+    }
+}
+
+ Write-Output "Keeping $($keeping) files/folders."
